19972d61e487adee650083daa72f8faebf102eed
Jose David Padilla Gamarra <djgamarra@uninorte.edu.co>
2018-04-18 01:10:42 -0500

    Avanzando en modulo de cartelera

--- src/Ventana/PrincipalUsuario/PrincipalUsuario.java
+++ src/Ventana/PrincipalUsuario/PrincipalUsuario.java
@@ -1,6 +1,7 @@
 package Ventana.PrincipalUsuario;
 
 import BaseDeDatos.Pelicula;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -8,6 +9,7 @@ import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -61,17 +63,34 @@ public class PrincipalUsuario implements Initializable {
 
     private Pane nuevoBanner(Pelicula pelicula) throws IOException {
         final Pelicula pel = pelicula;
+
         Parent parent = FXMLLoader.load(getClass().getResource("/Ventana/PrincipalUsuario/BannerPelicula.fxml"));
+
         final Pane p = (Pane) parent.lookup("#panel");
-        ((ImageView) p.getChildren().get(0)).setImage(new Image(new File(pelicula.getImagen()).toURI().toString()));
-        ((Label) p.getChildren().get(1)).setText(pelicula.getNombre());
-        ((Label) p.getChildren().get(2)).setText(pelicula.getAutor());
-        ((Label) p.getChildren().get(3)).setText(pelicula.getGenero());
-        p.setOnMouseClicked(event -> {
-            p.setStyle("" +
-                    "-fx-background-color: #ff6639;"
-            );
-        });
+        String style = "-fx-cursor: hand;";
+        ImageView portada = (ImageView) p.getChildren().get(0);
+        Label nombre = (Label) p.getChildren().get(1);
+        Label autor = (Label) p.getChildren().get(2);
+        Label genero = (Label) p.getChildren().get(3);
+
+        portada.setImage(new Image(new File(pelicula.getImagen()).toURI().toString()));
+        nombre.setText(pelicula.getNombre());
+        autor.setText(pelicula.getAutor());
+        genero.setText(pelicula.getGenero());
+
+        portada.setStyle(style);
+        nombre.setStyle(style);
+        autor.setStyle(style);
+        genero.setStyle(style);
+
+        EventHandler evt = (EventHandler<MouseEvent>) event -> {
+            System.out.println("click");
+        };
+
+        portada.setOnMouseClicked(evt);
+        nombre.setOnMouseClicked(evt);
+        autor.setOnMouseClicked(evt);
+        genero.setOnMouseClicked(evt);
         return p;
     }
 
