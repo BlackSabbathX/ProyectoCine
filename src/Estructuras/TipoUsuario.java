3faa42f41c83a14a580a316037fdf0326c77a398
Jose David Padilla Gamarra <djgamarra@uninorte.edu.co>
2018-05-01 22:54:33 -0500

    Trabajando en modulo admin

--- src/Estructuras/TipoUsuario.java
+++ src/Estructuras/TipoUsuario.java
@@ -11,7 +11,7 @@ public enum TipoUsuario {
     Administrador("Administrador") {
         @Override
         public String toString() {
-            return "Evaluador";
+            return "Administrador";
         }
     };
 
